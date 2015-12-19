63f7e857beb99bfb4de0f4259bc79d49fb44036d
Shijin Wang <Alanbition1@Shijins-MacBook-Pro.local>
2015-12-19 02:22:59 -0500

    Initial Commit

---
+++ CS490 prework/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CS490 prework
+//
+//  Created by Shijin Wang on 15/12/19.
+//  Copyright (c) 2015年 Alan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
